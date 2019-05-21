63a5f11e45e958ca0444809dcf882e61c0e7d636
Wiktoria Szramowska <irissiell@wp.pl>
2019-05-21 23:42:15 +0200

    fixed problem with css

--- src/main/java/com/exercise/calculator/controllers/CalculationController.java
+++ src/main/java/com/exercise/calculator/controllers/CalculationController.java
@@ -22,75 +22,75 @@ public class CalculationController {
         return "index";
     }
 
-    @PostMapping(params = "add")
+    @PostMapping(params = "add", value = "/")
     public String add(@ModelAttribute Data data, ModelMap map) {
         map.addAttribute("result", simpleCalculation.add(data.getA(), data.getB()));
         return "index";
     }
 
-    @PostMapping(params = "sub")
+    @PostMapping(params = "sub", value = "/")
     public String sub(@ModelAttribute Data data, ModelMap map) {
         map.addAttribute("result", simpleCalculation.subtraction(data.getA(), data.getB()));
         return "index";
     }
 
-    @PostMapping(params = "multi")
+    @PostMapping(params = "multi", value = "/")
     public String multi(@ModelAttribute Data data, ModelMap map) {
         map.addAttribute("result", simpleCalculation.multiplication(data.getA(), data.getB()));
         return "index";
     }
 
-    @PostMapping(params = "div")
+    @PostMapping(params = "div", value = "/")
     public String div(@ModelAttribute Data data, ModelMap map) {
         map.addAttribute("result", simpleCalculation.division(data.getA(), data.getB()));
         return "index";
     }
 
-    @PostMapping(params = "percent")
+    @PostMapping(params = "percent", value = "/")
     public String percent(@ModelAttribute Data data, ModelMap map) {
         map.addAttribute("result", simpleCalculation.percentOf(data.getA(), data.getB()));
         return "index";
     }
 
-    @PostMapping(params = "pow")
+    @PostMapping(params = "pow", value = "/")
     public String power(@ModelAttribute Data data, ModelMap map) {
         map.addAttribute("result", simpleCalculation.powerOf(data.getA(), data.getB()));
         return "index";
     }
 
-    @PostMapping(params = "modulo")
+    @PostMapping(params = "modulo", value = "/")
     public String modulo(@ModelAttribute Data data, ModelMap map) {
         map.addAttribute("result", simpleCalculation.modulo(data.getA(), data.getB()));
         return "index";
     }
 
     //-------------------------------------------------
-    @PostMapping(params = "sqr")
+    @PostMapping(params = "sqr", value = "/")
     public String sqr(@ModelAttribute Data data, ModelMap map) {
         map.addAttribute("result", otherCalculation.squareOf(data.getA()));
         return "index";
     }
 
-    @PostMapping(params = "fac")
+    @PostMapping(params = "fac", value = "/")
     public String factorial(@ModelAttribute Data data, ModelMap map) {
         map.addAttribute("result", otherCalculation.factorial(data.getA()));
         return "index";
     }
 
-    @PostMapping(params = "cub")
+    @PostMapping(params = "cub", value = "/")
     public String cubeOf(@ModelAttribute Data data, ModelMap map) {
         map.addAttribute("result", otherCalculation.cubeOf(data.getA()));
         return "index";
     }
 
-    @PostMapping(params = "binary")
+    @PostMapping(params = "binary", value = "/")
     public String binary(@ModelAttribute Data data, ModelMap map) {
         map.addAttribute("result", otherCalculation.showAsBinary(data.getA()));
         return "index";
     }
 
     //-------------------------------------------------
-    @PostMapping(params = "reset")
+    @PostMapping(params = "reset", value = "/")
     public String reset() {
         return "redirect:/";
     }
